9eb85f22a321a3876f5348865e3f3ac478ac44ca
Tsivka Galina <galinatsivka@gmail.come>
2020-07-09 17:38:13 +0300

    new commit

--- src/main/java/FootballMatch.java
+++ src/main/java/FootballMatch.java
@@ -13,16 +13,9 @@ public class FootballMatch {
      */
 
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        int win = ((result1 == bet1) && (result2 == bet2)) ? 2 : 0;
-        if (0 == win) {
-            win = (((result1 - result2) < 0) && ((bet1 - bet2) < 0)) ? 1 : 0;
-        }
-        if (0 == win) {
-            win = (((bet1 - bet2) > 0) && ((result1 - result2) > 0)) ? 1 : 0;
-        }
-        if (0 == win) {
-            win = ((result2 != result1) || (bet1 != bet2)) ? 0 : 1;
-        }
-        return win;
+        return ((result1 == bet1) && (result2 == bet2)) ? 2 :
+                ((((result1 > result2) && (bet1 > bet2))
+                        || ((result1 < result2) && (bet1 < bet2))
+                        || ((result1 == result2) && (bet1 == bet2))) ? 1 : 0);
     }
 }
